ab42ab0c4b5ab35b3a14c7b2702016584a052da7
afeiship <1290657123@qq.com>
2022-04-01 11:31:59 +0800

    refactor: udpate locale/dist

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-var fs = require('fs');
-var appRoot = require('app-root-path').path;
-var safeAssign = require('safe-assign');
-var path = require('path');
+const fs = require('fs');
+const safeAssign = require('safe-assign');
+const path = require('path');
 
 // next packages
 require('@jswork/next');
@@ -9,66 +8,59 @@ require('@jswork/next-baidu-fanyi');
 require('@jswork/next-async-queue');
 require('@jswork/next-list2map');
 
-/* prettier-ignore */
-var RETURN_VALUE = function(inValue) {
-  return inValue.data;
-};
-var defaults = {
-  cwd: appRoot,
+const defaults = {
+  cwd: process.cwd(),
   input: 'original.json',
   output: 'dist',
   tab: 2,
   from: 'zh',
-  lang: {
-    en: 'en',
-    zh: 'zh_CN',
-    cht: 'zh_TW'
-  },
-  filter: RETURN_VALUE,
+  to: [
+    { key: 'en', value: 'en' },
+    { key: 'zh', value: 'zh_CN' },
+    { key: 'cht', value: 'zh_TW' }
+  ],
+  filter: (v) => v.data,
   apiOptions: null
 };
 
 module.exports = function(inOptions) {
-  var options = nx.mix(null, defaults, inOptions);
-  var input = path.join(options.cwd, options.input);
-  var output = path.join(options.cwd, options.output);
-  var keys = Object.keys(require(input));
-  var requests = [];
-  var initialized = false;
+  const options = nx.mix(null, defaults, inOptions);
+  const input = path.join(options.cwd, options.input);
+  const output = path.join(options.cwd, options.output);
+  const keys = Object.keys(require(input));
+  let initialized = false;
 
   if (!fs.existsSync(output)) {
     fs.mkdirSync(output);
     initialized = true;
   }
 
-  // requets
-  nx.forIn(options.lang, function(key, value) {
-    var outputFile = path.join(output, value + '.json');
-    requests.push({
+  const requests = options.to.map(item => {
+    return {
       data: nx.mix(
         {
           q: keys.join('\n'),
           from: options.from,
-          to: key,
+          to: item.key,
           delay: 1200
         },
         options.apiOptions
       ),
-      output: outputFile
-    });
+      output: path.join(output, item.value + '.json')
+    };
   });
 
-  var items = requests.map(function(request) {
+  const items = requests.map(function(request) {
     return function(next) {
       return nx.BaiduFanyi.translate(request.data).then(function(res) {
-        var filtered = options.filter({ data: res.trans_result, config: request });
-        var old = null;
+        const filtered = options.filter({ data: res.trans_result, config: request });
+        let old = null;
         if (!initialized && fs.existsSync(request.output)) {
           old = require(request.output);
         }
-        var translated = nx.list2map(filtered, { key: 'src', value: 'dst' });
-        var data = safeAssign(translated, old);
-        var buffered = JSON.stringify(data, null, options.tab);
+        const translated = nx.list2map(filtered, { key: 'src', value: 'dst' });
+        const data = safeAssign(translated, old);
+        const buffered = JSON.stringify(data, null, options.tab);
         fs.writeFileSync(request.output, buffered);
         next();
       });
